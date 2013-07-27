4f82dcd9db625d1d9705468415c401a34956c43d
Bart Riemens <briemens@crafity.com>
2013-07-27 15:26:29 +0200

    Added crafity webserver tests to npm test script

--- scripts/npm-test-jstest.sh
+++ scripts/npm-test-jstest.sh
@@ -15,4 +15,9 @@ export ErrorCode=$?
 if [ "0" != "$ErrorCode" ]; then
 	export FailedTest=1
 fi
+node ./test/crafity.webserver.test.js
+export ErrorCode=$?
+if [ "0" != "$ErrorCode" ]; then
+	export FailedTest=1
+fi
 exit $FailedTest
